c72ddb5e5ca5cdaa0be9ba27095dcef37d7ac084
Ezzio Salas <esalas@zigatta.com>
2022-06-10 11:43:24 -0500

    styles:css ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
-                    {item.icon}
+                    <div  className="icon_navbar"> {item.icon}</div>
                     <span>{item.title}</span>
                   </Link>
                 </li>
